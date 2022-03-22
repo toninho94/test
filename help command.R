89a75829c3300fb643b9e37fea10cbbcd793cd16
toninho <guibotass@gmail.com>
2022-03-22 06:50:19 -0300

    Últimas alterações

--- help command.R
+++ help command.R
@@ -1,1 +1,15 @@
-? fipe_vehicle
+#? fipe_vehicle
+#??fipe_vehicle
+#help.search("fipe_vehicle")
+#help(fipe_vehicle)
+
+#help("pie")
+#example(pie)
+
+#? hist
+
+#?? "pearson correlation"
+
+#example(image)
+
+args(help)
